59bef90f0c5649cf850dfb6b460f2b7d2b2f1381
Jim Dale Abad <147327072+JimAbad@users.noreply.github.com>
2024-09-05 00:12:12 +0800

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,27 @@
+# sum of two numbers
+print("Sum of two numbers")
+first = input("First: ")
+second = input("Second: ")
+sum = int (first) + int(second)
+print (sum)
+
+# subtraction of two numbers
+print("Subtraction of two numbers")
+first = input("First: ")
+second = input("Second: ")
+sub = int (first) - int(second)
+print (sub)
+
+# multiplication of two numbers
+print("Multiplication of two numbers")
+first = input("First: ")
+second = input("Second: ")
+mul = int (first) * int (second)
+print (mul)
+
+# division of two numbers
+print("Division of two numbers")
+first = input("First: ")
+second = input("Second: ")
+div = int(first) / int (second)
+print (div)
